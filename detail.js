65cddb199f7870a9bac4818c25744964b804e78e
MrHate <dgyhateyou@sjtu.edu.cn>
2019-03-09 00:06:12 +0800

    before alternative css

---
+++ detail.js
@@ -0,0 +1,9 @@
+var detail_app = new Vue({
+	el: '#detail_app',
+	data:{
+		name: 'test book',
+		path: 'images/b1.jpeg',
+		subtitle: 'subtitle',
+		content: 'Download the Vue Devtools extension for a better development experience:https://github.com/vuejs/vue-devtools'
+	}
+})
