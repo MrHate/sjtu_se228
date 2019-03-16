3ec1ee011a5f8a05d733e80674a199e34cb3aa91
MrHate <dgyhateyou@sjtu.edu.cn>
2019-03-16 17:16:00 +0800

    detail.js removed

--- detail.js
+++
@@ -1,9 +0,0 @@
-var detail_app = new Vue({
-	el: '#detail_app',
-	data:{
-		name: 'test book',
-		path: 'images/b1.jpeg',
-		subtitle: 'subtitle',
-		content: 'Download the Vue Devtools extension for a better development experience:https://github.com/vuejs/vue-devtools'
-	}
-})
--- index.js
+++ index.js
@@ -13,19 +13,22 @@ var header_contents = new Vue({
 	el:"#header_contents"
 })
 
+var nav_menu = new Vue({
+	el:"#nav_menu"
+})
+
 window.onload = function() {
 	var imgsWrp = document.querySelector(".imgsWrp"),
 		imgsLi = imgsWrp.querySelectorAll("li"),
 		markSpan = document.querySelector(".carousel span"),
 		i = 0;
 
-	// 这里根据图片数量分别算出li的宽度和ul的宽度
 	imgsWrp.style.width = slider.imgsLens * 100 + "%";
 	var widthLi = 100 / slider.imgsLens + "%";
 	for (var ils = 0; ils < slider.imgsLens; ils++) {
 		imgsLi[ils].style.width = widthLi;
 	}
-	// 定时函数，每3秒变更i，同时变换ul要transform的值
+	
 	setInterval(function() {
 		i < slider.imgsLens - 1 ? i++ : i = 0;
 		imgsWrp.style.transform = "translateX(-" + 100 / slider.imgsLens * i + "%)";
