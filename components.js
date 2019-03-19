92c98064d584ba579f78c8812e15d40fda0f5f9e
MrHate <dgyhateyou@sjtu.edu.cn>
2019-03-19 10:30:17 +0800

    folder unused removed

--- components.js
+++ components.js
@@ -1,37 +1,15 @@
 Vue.component('header-contents',{
 	data:function(){
 		return{
-			showDialog:false,
-			isLoggedIn:false,
-			showAlert:false,
-			username:"",
+			username:"test",
 			password:""
 		}
 	},
 	methods:{
-		showModal:function(){
-			this.showDialog = true;
-		},
-		loginFun:function(){
-			this.username = this.$refs.username_input.value;
-			this.password = this.$refs.password_input.value;
-			if(this.username == "" || this.password == ""){
-				this.showAlert = true;
-				return;
-			}
-			this.showAlert = false;
-			this.showDialog = false;
-			this.isLoggedIn = true;
-			console.log("user: "+this.username+"\npassword: "+this.password);
-		},
-		signupFun:function(){
-			alert(this.$refs.username_input.value);
-		},
 		logoutFun:function(){
-			this.showDialog = false;
 			this.username = "";
 			this.password = "";
-			this.isLoggedIn = false;
+			alert("log out");
 		}
 	},
 	template:'\
@@ -46,20 +24,7 @@ Vue.component('header-contents',{
 			</div>\
 			<div class="container-fluid">\
 				<div class="pull-right">\
-					<div>\
-						<div v-if="isLoggedIn">{{username}}<a href="#" @click="logoutFun">Log out</a></div>\
-						<div v-else=""><a class="nav_btn" href="#" @click="showModal">Sign up/Login</a></div>\
-						<div class="login_modal" v-show="showDialog">\
-							<p>test dialog</p>\
-							<form>\
-								 User:<br><input type="text" name="username" ref="username_input">\
-								 Password:<br><input type="text" name="password" ref="password_input">\
-								<button type="button" @click="loginFun">log in</button>\
-								<button type="button" @click="signupFun">sign up</button>\
-							</form>\
-							<div v-show="showAlert"><br>Username and password can not be empty.</div>\
-						</div>\
-					</div>\
+					<div>{{username}}<a href="#" @click="logoutFun">Log out</a></div>\
 				</div>\
 			</div>\
 		</div>'
