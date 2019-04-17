b7ee570b25980e3b5a1b75aae0966c0d6a33abd5
MrHate <dgyhateyou@sjtu.edu.cn>
2019-04-17 18:15:05 +0800

    with spring security works

---
+++ src/main/java/com/dgy/ebook/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.dgy.ebook;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+ 
+@Configuration 
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private UserDetailsService userDetailsService;
+	 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests()
+			//.antMatchers("/actuator").hasAuthority("SUPER_ADMIN")
+			//.antMatchers("/actuator/**").hasAuthority("SUPER_ADMIN")
+			.antMatchers("/login").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+			.loginPage("/login").permitAll()
+			.and()
+			.logout().permitAll();
+	 
+	}
+}
